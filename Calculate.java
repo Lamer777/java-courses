4e7c449d8bfaf4f9965a8757552eac65706b03ac
Lamer777 <artgtx@gmail.com>
2017-10-10 23:28:00 +0700

    Add func 4 operations with 2 var

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,15 @@
 public class Calculate{
 	public static void main(String[] arg){
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		int minus = first - second;
+		int multi = first * second;
+		int seg = first / second;					
+		System.out.println("Sum " + summ);
+		System.out.println("First - second " + minus);
+		System.out.println("First * second " + multi);
+		System.out.println("First / second " + seg);
 	}
 }
